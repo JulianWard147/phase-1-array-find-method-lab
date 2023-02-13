d97804f200a4bbded442078ecf7b97bd62c1c46d
Julian Ward <wardjulianm@gmail.com>
2023-02-13 10:50:28 -0700

    Go Broncos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+
+
+function isSBWin(element, index, array) {
+
+    return(element.result === "W")
+}
+
 // code your solution here
+function superbowlWin(teamDataArray){
+    if (typeof teamDataArray.find(isSBWin) !== 'undefined'){
+      return teamDataArray.find(isSBWin).year}
+    else{
+        return} 
+}
+
+
